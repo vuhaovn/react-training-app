c432b8578894e41d452b7270e60b1849fc9bb344
Vu Hao <theflash8888@gmail.com>
2021-09-01 11:01:03 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -95,14 +95,14 @@ function App() {
           <nav>
             <ul>
               <li>
-                <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+                <NavLink to='https://vuhaovn.github.io/react-training-app/' activeClassName='active'>Home</NavLink>
               </li>
               <li>
-                <NavLink exact to='/about' activeClassName='active'>About</NavLink>
+                <NavLink to='https://vuhaovn.github.io/react-training-app/about' activeClassName='active'>About</NavLink>
               </li>
             </ul>
           </nav>
-          <Route exact path='/'>
+          <Route path='https://vuhaovn.github.io/react-training-app/'>
             <Header onShow={onShow} show={show} />
             {show && <AddTask onAdd={addTask} />}
             <Search onSearch={onSearch}/>
@@ -113,7 +113,7 @@ function App() {
             onToggle={toggleTask}
             /> : <p>No task to show</p>}
           </Route>
-          <Route exact path='/about'>
+          <Route path='https://vuhaovn.github.io/react-training-app/about'>
             <About />
           </Route>
           <Footer />
